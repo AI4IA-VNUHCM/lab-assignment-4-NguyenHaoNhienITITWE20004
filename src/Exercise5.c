1d6962876e196450d7a72c261f6e7aee3362f47e
NguyenHaoNhienITITWE20004 <91869151+NguyenHaoNhienITITWE20004@users.noreply.github.com>
2021-10-25 16:49:50 +0700

    Update Exercise5.c

--- src/Exercise5.c
+++ src/Exercise5.c
@@ -37,7 +37,31 @@ void Ex5(int arr[], int m, int n){
 	int a[SIZE][SIZE];
 	Array2Dconverter(arr,a,m,n);
 	//Your codes here
+int column, row;
+	for(column = 0; column < n; column++)
+	{
+	for(row = 0; row < m; row++){
+		for(int sort = row + 1; sort < m; sort++){
+			if(a[row][column] > a[sort][column]){
+				int temp = a[row][column];
+				a[row][column] = a[sort][column];
+				a[sort][column] = temp;
 
+			}
+		}
+		 int max;
+    max=a[0][0];
+    	for(row = 0; row < m; row++)
+    {
+        if(a[row][0]>max)
+        {
+            max=a[row][0];
+            printf("\n%d",max);
+        }
+    }
+	}
+	}
+ Array2Dconverter(arr, a, m, n);
 }
 
 int main(int argc, char *argv[]) {
